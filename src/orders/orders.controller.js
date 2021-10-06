9200cdc3560de83dff9eaa6bd8c9cf338c71f332
Jonathan Borofsky <jonborofsky@gmail.com>
2021-10-06 15:47:27 -0500

    66/68 passing

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const { update } = require("../dishes/dishes.controller");
 
 // Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
@@ -7,65 +6,92 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+//  Middleware
+
+function statusIsValid(req, res, next) {
+  const { data } = req.body;
+  const { id, status } = data;
+
+  if (
+    !status ||
+    status === "" ||
+    status !== "pending"
+  ) {
+    return next({
+      status: 400,
+      message: `Order must have a status of pending, preparing, out-for-delivery, delivered`,
+    });
+  } else if (status === "delivered") {
+    return next({
+      status: 400,
+      message: `A delivered order cannot be changed`,
+    });
+  }
+  next();
+}
+
+function updateOrderIdIsValid(req, res, next) {
+  const { orderId } = req.params;
+  const { data } = req.body;
+  const { id } = data;
+  if (id && orderId !== data.id) {
+    return next({
+      status: 400,
+      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}.`,
+    });
+  }
+  next();
+}
 
 function orderHasValidFields(req, res, next) {
-    const { data = {} } = req.body;
-    const { deliverTo, mobileNumber, dishes } = data;
-    const VALID_FIELDS = ["deliverTo", "mobileNumber", "dishes"];
-  
-    //Are all required fields present
-    for (const field of VALID_FIELDS) {
-      if (!data[field]) {
-        return next({
-          status: 400,
-          message: `Order must include a ${field}`,
-        });
-      }
+  const { data = {} } = req.body;
+  const { deliverTo, mobileNumber, dishes } = data;
+  const VALID_FIELDS = ["deliverTo", "mobileNumber", "dishes"];
+
+  //Are all required fields present
+  for (const field of VALID_FIELDS) {
+    if (!data[field]) {
+      return next({
+        status: 400,
+        message: `Order must include a ${field}`,
+      });
     }
-  
-    //Is dish fields are not valid
+  }
+  //If dish fields are not valid
+  if (
+    deliverTo === "" ||
+    mobileNumber === "" ||
+    !Array.isArray(dishes) ||
+    dishes.length <= 0
+  ) {
+    return next({
+      status: 400,
+      message: `Order must include at least one dish`,
+    });
+  }
+  //iterate through dish key and validate fields
+  for (const dish of dishes) {
     if (
-      deliverTo === "" ||
-      mobileNumber === "" ||
-      !Array.isArray(dishes) ||
-      dishes.length <= 0
+      !dish.quantity ||
+      !Number.isInteger(dish.quantity) ||
+      Number(dish.quantity) === 0
     ) {
       return next({
         status: 400,
-        message: `Order must include at least one dish`,
+        message: `Dish ${dishes.indexOf(
+          dish
+        )} must have a quantity that is an integer greater than 0`,
       });
     }
-  
-    for (const dish of dishes) {
-      if (
-        !dish.quantity ||
-        !Number.isInteger(dish.quantity) ||
-        Number(dish.quantity) === 0
-      ) {
-        return next({
-          status: 400,
-          message: `Dish ${dishes.indexOf(dish)} must have a quantity that is an integer greater than 0`,
-        });
-      }
-
-    }
-    next();
   }
-
-
-function list(req, res, next) {
-  res.json({
-    data: orders,
-  });
+  next();
 }
-
 
 function orderExists(req, res, next) {
   const { orderId } = req.params;
-  const foundOrder = dishes.find((order) => order.id === orderId);
+  const foundOrder = orders.find((order) => order.id === orderId);
 
-  if (foundDish) {
+  if (foundOrder) {
     res.locals.order = foundOrder;
     next();
   }
@@ -77,24 +103,64 @@ function orderExists(req, res, next) {
 
 //CRUD + list
 
-const create = (req, res, next) => {
-    const newOrder = {
-      id: nextId(),
-      ...req.body.data,
-    };
-    orders.push(newOrder);
-    res.status(201).json({ data: newOrder });
-  };
+function list(req, res, next) {
+  res.json({
+    data: orders,
+  });
+}
 
-  function read(req, res) {
-    const foundOrder = res.locals.order
-    res.json({ data: foundOrder });
+const create = (req, res, next) => {
+  const newOrder = {
+    id: nextId(),
+    ...req.body.data,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+};
+
+function read(req, res) {
+  const foundOrder = res.locals.order;
+  res.json({ data: foundOrder });
+}
+
+function update(req, res, nect) {
+  const { order } = res.locals;
+  const { data: update } = req.body;
+  for (let prop in update) {
+    if (update[prop]) {
+      order[prop] = update[prop]
+    }
   }
+  res.json({ data: order })
+}
+
+function destroy(req, res, next) {
+  const { orderId } = res.params
+  const { status } = req.locals.order
+
+  if (status === "pending") {
+    const index = orders.findIndex((order) => order.id === orderId);
+
+    orders.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    next({
+      status: 400,
+      message: `An order cannot be deleted unless it is pending`
+    })
+  }
+}
 
 module.exports = {
   list,
   create: [orderHasValidFields, create],
   read: [orderExists, read],
-  update,
-  //destroy,
+  update: [
+    orderExists,
+    orderHasValidFields,
+    updateOrderIdIsValid,
+    statusIsValid,
+    update,
+  ],
+  delete: [orderExists, destroy],
 };
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const controller = require("../orders/orders.controller")
+const controller = require("./orders.controller")
 const methodNotAllowed = require("../errors/methodNotAllowed")
 
 // TODO: Implement the /orders routes needed to make the tests pass
@@ -14,7 +14,7 @@ router
     .route("/:orderId")
     .get(controller.read)
     .put(controller.update)
-    //.delete(controller.delete)
+    .delete(controller.delete)
     .all(methodNotAllowed)
 
 module.exports = router;
